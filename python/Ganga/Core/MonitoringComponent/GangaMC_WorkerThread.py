29c6ed144743861622c20fe1d9bba1201dbe22af
Robert A Currie <rcurrie@cern.ch>
2016-02-01 20:54:15 +0000

    Moving Worker threads from inside the Monitoring Loop

---
+++ python/Ganga/Core/MonitoringComponent/GangaMC_WorkerThread.py
@@ -0,0 +1,66 @@
+import Queue
+import threading
+
+from Ganga.Core.GangaThread import GangaThread
+
+from Ganga.GPIDev.Base.Proxy import isType, stripProxy, getName, getRuntimeGPIObject
+
+# Setup logging ---------------
+from Ganga.Utility.logging import getLogger
+
+from Ganga.Core.MonitoringComponent.UpdateDict import JobAction
+
+class MonitoringWorkerThread(GangaThread):
+
+    def __init__(self, name):
+        GangaThread.__init__(self, name)
+
+    def run(self):
+        self._execUpdateAction()
+
+    # This function takes a JobAction object from the Qin queue,
+    # executes the embedded function and runs post result actions.
+    def _execUpdateAction(self):
+        # DEBUGGING THREADS
+        # import sys
+        # sys.settrace(_trace)
+        while not self.should_stop():
+            log = getLogger()
+            log.debug("%s waiting..." % threading.currentThread())
+            #setattr(threading.currentThread(), 'action', None)
+
+            from Ganga.Core.MonitoringComponent.Local_GangaMC_Service import Qin
+
+            while not self.should_stop():
+                try:
+                    action = Qin.get(block=True, timeout=0.5)
+                    break
+                except Queue.Empty:
+                    #pass
+                    continue
+                finally:
+                    pass
+            if self.should_stop():
+                break
+
+            #setattr(threading.currentThread(), 'action', action)
+            log.debug("Qin's size is currently: %d" % Qin.qsize())
+            log.debug("%s running..." % threading.currentThread())
+
+            if not isType(action, JobAction):
+                continue
+            if action.function == 'stop':
+                break
+            try:
+                result = action.function(*action.args, **action.kwargs)
+            except Exception as err:
+                log.debug("_execUpdateAction: %s" % str(err))
+                action.callback_Failure()
+            #finally:
+            #    pass
+            else:
+                if result in action.success:
+                    action.callback_Success()
+                else:
+                    action.callback_Failure()
+
