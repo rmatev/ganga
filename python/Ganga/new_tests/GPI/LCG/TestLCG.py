0b5b91001da5364e9e529d6ebf8069bf693b5f2f
Matt Williams <matt@milliams.com>
2016-02-10 18:24:49 +0000

    Add our own glite config file for the gridpp VO

--- python/Ganga/new_tests/GPI/LCG/TestLCG.py
+++ python/Ganga/new_tests/GPI/LCG/TestLCG.py
@@ -1,3 +1,5 @@
+import os
+
 from GangaUnitTest import GangaUnitTest
 
 class TestLCG(GangaUnitTest):
@@ -7,6 +9,11 @@ class TestLCG(GangaUnitTest):
         from Ganga.GPI import gridProxy
         gridProxy.voms = 'gridpp'
         gridProxy.create()
+
+        from Ganga.Utility.Config import setConfigOption
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        conf_file = os.path.join(current_dir, 'glite_wmsui.conf')
+        setConfigOption('LCG', 'Config', conf_file)
 
     def testJobSubmit(self):
         from Ganga.GPI import Job, LCG
